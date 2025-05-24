159f0a28964b234feb73fe4d35cffec47b4bcef0
freebirdyeah <mr.shaanchopra@gmail.com>
2025-05-25 02:00:10 +0530

    essential changes I rm -rf ed

--- src/micronn/__init__.py
+++ src/micronn/__init__.py
@@ -1,5 +1,16 @@
+__version__ = "0.1.0"
+
+# core API
 from .model import Model, Dense
+
+# activations
 from .activations import sigmoid, relu, tanh, softmax
+
+# losses
 from .loss import MSE
 
-__all__ = ['Model', 'Dense', 'sigmoid', 'relu', 'tanh', 'softmax', 'MSE']
+__all__ = [
+    "Model", "Dense",
+    "sigmoid", "relu", "tanh", "softmax",
+    "MSE",
+]
